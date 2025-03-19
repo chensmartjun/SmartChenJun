e5cbb114a13a6a0674af9e64f15b73b19def1995
chensmartjun <chjun1314@icloud.com>
2025-03-19 02:06:40 -0700

    Add files via upload

---
+++ src/views/HomePage.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home-container">
+    <h2>欢迎使用 ERP 系统</h2>
+    <p>数据可视化功能将在系统开发完成后实现</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomePage',
+};
+</script>
+
+<style scoped>
+.home-container { padding: 20px; text-align: center; }
+</style>
---
+++ src/views/Login.vue
@@ -0,0 +1,17 @@
+<!-- src/views/Login.vue -->
+<template>
+  <div class="login">
+    <h2>登录</h2>
+    <el-button type="primary" @click="$router.push('/')">返回首页</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LoginPage',
+};
+</script>
+
+<style scoped>
+.login { text-align: center; padding: 50px; }
+</style>
